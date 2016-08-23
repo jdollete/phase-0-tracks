cf4d8267d4358f786c0587b1aa228175e18dc1fd
Allain Dollete <allain.dollete@gmail.com>
2016-08-23 18:11:08 -0400

    Change number strings to intergers and print out data to console

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,7 +2,8 @@ puts "What is the hamster's name?"
 ham_name = gets.chomp
 
 puts "What is your volume level? (1-10)"
-ham_volume = gets.chomp
+ham_volume = gets.chomp.to_i
+
 
 puts "What is the hamster's fur color?"
 ham_color = gets.chomp
@@ -10,11 +11,18 @@ ham_color = gets.chomp
 puts "Is this hamster a good candidate for adoption? (Y/N)"
 ham_adopt = gets.chomp
 
-puts "Estimated age of hamster?"
-ham_age = gets.chomp
+puts "Age of hamster?"
+ham_age = gets.chomp.to_i
 
-	if ham_age == ""
+	if ham_age == 0
 		ham_age = nil
+
 	else
-		puts ham_age
+		ham_age = ham_age
 	end
+
+puts "Name: #{ham_name}"
+puts "Volume: #{ham_volume}"
+puts "Fur Color: #{ham_color}"
+puts "Adoptable: #{ham_adopt} "
+puts "Age: #{ham_age}"
