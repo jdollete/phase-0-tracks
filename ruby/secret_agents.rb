9066f812fab7d267c0d4de872188d140a8ccdcbc
Allain Dollete <allain.dollete@gmail.com>
2016-08-26 17:32:31 -0400

    Insert interface for agent

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,9 +1,7 @@
+# Method Code
+
 # Encrypt
 # Get user password
-
-puts "Insert Password"
-user_pass = gets.chomp
-
 # Get word length
 # Counter starts at 0, while counter is less than length keep looping indexes
 # Will call .next on each character
@@ -12,11 +10,14 @@ user_pass = gets.chomp
 # Puts the new password
 def encryptor(user_pass)
 	counter = 0
-	output = " "
+	output = ""
 	while counter < user_pass.length
 
 	  if user_pass[counter] == " "
 	  	output += " "
+    elsif user_pass[counter] == "z"
+      # conditional for "z"
+      output += "a"
 	  else
 	  	output += user_pass[counter].next
 	  end
@@ -25,11 +26,8 @@ def encryptor(user_pass)
 
 	end
 
-	p output
+	return output
 end
-
-encryptor(user_pass)
-
 
 
 # Decrypt
@@ -37,12 +35,10 @@ encryptor(user_pass)
 # Counter starts at 0, while counter is less than length keep looping indexes
 # Set variable to alphabet with string input a-z
 # Go through each character in alphabet string, check the index of current letter against it
-puts "Password to decrypt:"
-user_decpt = gets.chomp.downcase
 
 def decryptor(user_decpt)
 	counter = 0
-	output = " "
+	output = ""
 	alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 	while counter < user_decpt.length
@@ -50,21 +46,48 @@ def decryptor(user_decpt)
 		if user_decpt[counter] == " "
 			output += " "
 		else
-# While
+
 			temp = user_decpt[counter]
 			# puts alphabet.index(temp)
 # Check alphabet index counter
 			ind = alphabet.index(temp)
 			output += alphabet[ind-1]
-			# alphabet.index(temp)
-
 
 		end
 
 		counter += 1
 	end
-puts output
+return output
 
 end
 
-decryptor(user_decpt)
+# DRIVER CODE
+
+ # encryptor("abc") # should return "bcd"
+ # encryptor("zed") # should return "afe"
+ # decryptor("bcd") # should return "abc"
+ # decryptor("afe") # should return "zed"
+
+ # puts decryptor(encryptor("swordfish"))
+ # Nested methods work because they work from the inside out!
+
+ # Release 5: Add an Interface
+
+ # Print a prompt to the screen, ask if agent is encrypting or decrypting
+ # Us an if/else statement to determine which method to call
+ # Get input from agent
+
+ puts "Agent, are you encrypting or decrypting? [e/d]"
+ entry = gets.chomp
+
+ if entry == "e"
+   puts "Enter password to encrypt:"
+   user_pass = gets.chomp.downcase
+   p encryptor(user_pass)
+ elsif entry == "d"
+   puts "Enter password to decrypt:"
+   user_decpt = gets.chomp.downcase
+   p decryptor(user_decpt)
+ else
+   puts "invalid entry you are a bad agent!!!!"
+ end
