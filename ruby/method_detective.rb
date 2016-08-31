418b7c73f73da486af47620e923227947d5ef2b5
Allain Dollete <allain.dollete@gmail.com>
2016-08-31 12:42:07 -0400

    Input answers to questions

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -6,34 +6,47 @@
 
 # "iNvEsTiGaTiOn".<???>
 # => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
 
 # "zom".<???>
 # => “zoom”
+"zom".insert(1, "o")
+
 
 # "enhance".<???>
 # => "    enhance    "
+"enchance".center(16)
 
 # "Stop! You’re under arrest!".<???>
 # => "STOP! YOU’RE UNDER ARREST!"
+"Stop! You’re under arrest!".upcase
 
 # "the usual".<???>
 #=> "the usual suspects"
+"the usual" << " suspects"
+"the usual".insert(9, ' suspects')
 
 # " suspects".<???>
 # => "the usual suspects"
+"suspects".insert(0, 'the usual suspects ')
 
 # "The case of the disappearing last letter".<???>
 # => "The case of the disappearing last lette"
+"The case of the disappearing last letter".chop
 
 # "The mystery of the missing first letter".<???>
 # => "he mystery of the missing first letter"
+"The mystery of the missing first letter".slice(1..-1)
 
 # "Elementary,    my   dear        Watson!".<???>
 # => "Elementary, my dear Watson!"
+"Elementary,    my   dear        Watson!".squeeze
 
 # "z".<???>
-# => 122 
+# => 122
 # (What is the significance of the number 122 in relation to the character z?)
+"z".ord
 
 # "How many times does the letter 'a' appear in this string?".<???>
 # => 4
+"How many times does the letter 'a' appear in this string?".count('a')
