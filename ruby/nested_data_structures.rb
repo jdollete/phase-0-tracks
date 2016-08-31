1e870b3dfd4c1b10ef0e91567c6b5d41efa38047
Allain Dollete <allain.dollete@gmail.com>
2016-08-31 15:19:12 -0400

    Start nested data structure with motorcycle shop

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,34 @@
+# Nested motorcycle shop
+
+m_shop = {
+  sport_bike: {
+    kawasaki: {
+      sport: ['Ninja 1000', 'Ninja 650', 'Ninja 300'],
+      super_sport: ['Ninja ZX-14R', 'Ninja ZX-10R', 'Ninja ZX-6R']
+    },
+    honda:
+    suzuki:
+    yamaha:
+    bmw:
+  },
+  cruiser: {
+    harley:
+    suzuki:
+    honda:
+    kawasaki:
+  },
+  atv: {
+    arctic_cat:
+    suzuki:
+    yamaha:
+    kawasaki:
+  },
+  maintainance: ['Oil Change', 'Inspection', 'Repairs', 'Modify']
+  },
+  assessories: {
+    helmets:
+    jackets:
+    boots:
+    gloves:
+  },
+}
