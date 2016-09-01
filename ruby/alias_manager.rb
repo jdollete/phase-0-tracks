55647f1ccf0aad829257f8bd7a55d4432d51326e
Allain Dollete <allain.dollete@gmail.com>
2016-08-31 20:58:08 -0400

    Add iteration

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,10 +4,31 @@
 # Iterate through letters to see if vowel, if it is a vowel go to the next vowel
 
 puts "Agent, what is your name?"
-agent_name = gets.chomp
+agent_name = "Allain Dollete"
 
-agent_rev= agent_name.split(' ').reverse.join(' ')
+agent_rev= agent_name.downcase.split(' ').reverse.join(' ').split('')
 
 p agent_rev
 
-agent_rev.map! { |letter|}
+agent_alias = []
+i = 0
+agent_rev.each do |letter|
+	vowels = ['a', 'e', 'i', 'o', 'u']
+
+  if letter == vowels[i]
+  	p letter
+    letter = vowels[i + 1]
+    p letter
+    agent_alias << letter
+    i += 1
+
+  #elsif letter == vowels[4]
+
+  else
+	agent_alias << letter
+  end
+end
+
+
+
+p agent_alias.join('').split(' ').map { |i| i.capitalize}.join(' ')
