4c9cd0de34f8ec5ade54649ab907426e9f1a2435
Allain Dollete <allain.dollete@gmail.com>
2016-09-01 20:56:02 -0400

    Create inferface for user until they input quit

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,39 +2,39 @@
 # Name is split into two words
 # Reverse/join words
 # Iterate through letters to see if vowel, if it is a vowel go to the next vowel
+until agent_name == "quit"
+	puts "Agent, what is your name?"
+	agent_name = "Allain Dollete"
 
-puts "Agent, what is your name?"
-agent_name = "Allain Dollete"
+	agent_rev= agent_name.downcase.split(' ').reverse.join(' ').split('')
 
-agent_rev= agent_name.downcase.split(' ').reverse.join(' ').split('')
+	p agent_rev
 
-p agent_rev
+	agent_alias = []
+	vowels = ['a', 'e', 'i', 'o', 'u']
+	i = 0
 
-agent_alias = []
-vowels = ['a', 'e', 'i', 'o', 'u']
-i = 0
+	agent_rev.each do |letter|
 
-agent_rev.each do |letter|
-
-  if letter == 'a' || 'e' || 'i' || 'o' || 'u'
-  	p letter
-  	i_v = vowels.index(letter)
-  	letter = vowels[i_v + 1]
-    p letter
-    agent_alias.insert(i, letter)
+	  if letter == 'a' || 'e' || 'i' || 'o' || 'u'
+	  	p letter
+	  	letter = vowels[vowels.index(letter) + 1]
+	    p letter
+	    agent_alias.insert(i, letter)
 
 
-  elsif letter == 'u'
-  	agent_alias.insert(i, vowels[0])
+	  elsif letter == 'u'
+	  	agent_alias.insert(i, vowels[0])
 
-  else
-	agent_alias.insert(i, letter)
+	  else
+		agent_alias.insert(i, letter)
 
-  end
-  i += 1
+	  end
+	  i += 1
 
+	end
+
+
+	p agent_alias
+	p agent_alias.join('').split(' ').map { |i| i.capitalize}.join(' ')
 end
-
-
-
-p agent_alias.join('').split(' ').map { |i| i.capitalize}.join(' ')
