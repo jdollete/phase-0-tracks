22a9b3b551ecc705c6b041d32a9d3e0d175fafc7
Allain Dollete <allain.dollete@gmail.com>
2016-09-02 14:58:27 -0400

    Create fibonacci method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,5 +1,7 @@
-number_array = [23, 5, 28, 87, 4, 8, 4, 10]
 
+
+# Method that seaches an array and
+# gives back the index number of value found
 def  search_array (array, number)
   counter = 0
   index = []
@@ -11,7 +13,37 @@ index.empty? ? nil : index
 end
 
 
-p search_array(number_array, 28)
-p search_array(number_array, 87)
-p search_array(number_array, 4)
-p search_array(number_array, 100)
+def fib(terms)
+  fib_terms = []
+  if terms == 1
+    fib_terms << 0
+  elsif terms == 2
+    fib_terms << 0 << 1
+  else
+    counter = 2
+    fib_terms << 0 << 1
+    while counter != terms
+      fib_terms << fib_terms[-1] + fib_terms[-2]
+      counter += 1
+    end
+  end
+  return fib_terms
+end
+
+
+# Driver code
+
+# Search array print
+# number_array = [23, 5, 28, 87, 4, 8, 4, 10]
+# p search_array(number_array, 28)
+# p search_array(number_array, 87)
+# p search_array(number_array, 4)
+# p search_array(number_array, 100)
+
+
+# Fibonacci print
+p fib(1)
+p fib(2)
+p fib(3)
+p fib(10)
+p fib(100)
