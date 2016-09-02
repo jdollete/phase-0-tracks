62bc40dc72db79d951394e68a37419c5b1639714
Allain Dollete <allain.dollete@gmail.com>
2016-09-01 21:13:28 -0400

    Fix conditional statement inside iteration

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,14 +1,19 @@
 # Agent inputs name
 # Name is split into two words
 # Reverse/join words
+# Split each characters to individual array elements
 # Iterate through letters to see if vowel, if it is a vowel go to the next vowel
+# If it isn't a vowel, just add to the array
+# Join array and split into two words, capitalize the first letter of each words
+# Combine both array elements into Combine
+# Until user inputs 'quit' keep asking for names
+
+puts "Agent, what is your name?"
+agent_name = gets.chomp
+
 until agent_name == "quit"
-	puts "Agent, what is your name?"
-	agent_name = "Allain Dollete"
 
 	agent_rev= agent_name.downcase.split(' ').reverse.join(' ').split('')
-
-	p agent_rev
 
 	agent_alias = []
 	vowels = ['a', 'e', 'i', 'o', 'u']
@@ -16,12 +21,21 @@ until agent_name == "quit"
 
 	agent_rev.each do |letter|
 
-	  if letter == 'a' || 'e' || 'i' || 'o' || 'u'
-	  	p letter
+	  if letter == 'a'
 	  	letter = vowels[vowels.index(letter) + 1]
-	    p letter
 	    agent_alias.insert(i, letter)
 
+	  elsif letter == 'e'
+	  	letter = vowels[vowels.index(letter) + 1]
+	    agent_alias.insert(i, letter)
+
+	   elsif letter == 'i'
+	  	letter = vowels[vowels.index(letter) + 1]
+	    agent_alias.insert(i, letter)
+
+	  elsif letter == 'o'
+	  	letter = vowels[vowels.index(letter) + 1]
+	    agent_alias.insert(i, letter)
 
 	  elsif letter == 'u'
 	  	agent_alias.insert(i, vowels[0])
@@ -34,7 +48,8 @@ until agent_name == "quit"
 
 	end
 
-
-	p agent_alias
 	p agent_alias.join('').split(' ').map { |i| i.capitalize}.join(' ')
+
+	puts "Agent, what is your name? (Or enter quit to exit program)"
+	agent_name = gets.chomp
 end
