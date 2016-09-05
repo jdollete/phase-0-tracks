1fe96ae30975720ae183e08cf58a059071b3b17e
Allain Dollete <allain.dollete@gmail.com>
2016-09-05 16:37:19 -0400

    Have user input his/her equation

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -17,10 +17,17 @@ end
 
 
 # driver code
-puts "Declare your first number: "
-num1 = gets.to_i
-puts "Declare your operator: "
-operator = gets.chomp
-puts "Declare your second number: "
-num2 = gets.to_i
+# puts "Declare your first number: "
+# num1 = gets.to_i
+# puts "Declare your operator: "
+# operator = gets.chomp
+# puts "Declare your second number: "
+# num2 = gets.to_i
+puts "What is your calculation?"
+user_inp = gets.chomp.gsub(/\s+/, "").split('')
+num1 = user_inp[0].to_i
+operator = user_inp[1]
+num2 = user_inp[2].to_i
+
 calculate(num1, operator, num2)
+# calculate(4,'+',5)
