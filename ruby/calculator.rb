893e3eb38614d616bee051c4e35dd1aaa062cbf8
Allain Dollete <allain.dollete@gmail.com>
2016-09-05 17:17:08 -0400

    Have it print out the input history

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -1,38 +1,43 @@
 # method code
 
-def calculate(int1, opr, int2)
-  if opr == "+"
-    p int1 + int2
-  elsif opr == "-"
-    p int1 - int2
-  elsif opr == "*"
-    p int1 * int2
-  elsif opr == "/"
-    p int1 / int2
-  else
-    puts "Invalid Entry"
+def calculate(user_inp)
+	i = 0
+	calc_hist = []
+  while user_inp != 'done'
+    user_calc = user_inp.gsub(/\s+/, "").split('')
+    num1 = user_calc[0].to_i
+    operator = user_calc[1]
+    num2 = user_calc[2].to_i
+
+      if operator == "+"
+        p sol = num1 + num2
+      elsif operator == "-"
+        p sol = num1 - num2
+      elsif operator == "*"
+        p sol = num1 * num2
+      elsif operator == "/"
+        p sol = num1 / num2
+      else
+        puts "Invalid Entry"
+      end
+      calc_hist.insert(i, "#{num1} #{operator} #{num2} = #{sol}")
+      i += 1
+	  puts "What is your calculation?"
+	  user_inp = gets.chomp
+  end
+
+  puts "#{i} calculations performed"
+  calc_hist.each do |equation|
+  	p equation
   end
 
 end
 
 
 # driver code
-# puts "Declare your first number: "
-# num1 = gets.to_i
-# puts "Declare your operator: "
-# operator = gets.chomp
-# puts "Declare your second number: "
-# num2 = gets.to_i
+
 puts "What is your calculation?"
 user_inp = gets.chomp
+calculate(user_inp)
 
-while user_inp != 'done'
-  user_calc = user_inp.gsub(/\s+/, "").split('')
-  num1 = user_calc[0].to_i
-  operator = user_calc[1]
-  num2 = user_calc[2].to_i
-  calculate(num1, operator, num2)
-  puts "What is your calculation?"
-  user_inp = gets.chomp
-end
 # calculate(4,'+',5)
