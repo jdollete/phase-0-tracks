fa088535673f2cb72228c1357a11062f4dae7e21
Allain Dollete <allain.dollete@gmail.com>
2016-09-05 22:06:35 -0400

    Create pseudocode for grocery list app

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,35 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps:
+  # Create an empty list to populate
+  # set default quantity
+  # Create variable set to zero
+  # print the list to the console [can you use one of your other methods here?]
+# output: [what data type goes here, array or hash?]
+  # Hash
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps:
+  # Split each word into an index that creates an array
+  # Turn indexes into keys
+  # Set the values to our default variables, all start at zero
+# output: Hash with items and values of zero
+
+# Method to remove an item from the list
+# input: item name
+# steps:
+  # Use delete method to remove item from hash
+# output: Hash
+
+# Method to update the quantity of an item
+# input: Item we want to replace and the new quantity
+# steps:
+  # Set new value to new input key
+# output: Hash
+
+# Method to print a list and make it look pretty
+# input: Hash Name
+# steps:
+ # Iterate through the list to print out each item and quantity
+# output: 
