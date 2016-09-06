45b00ec340c7a8a0af6fa657f717843b80eb0e56
Allain Dollete <allain.dollete@gmail.com>
2016-09-06 16:24:00 -0400

    Create puppy class list

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# Species
+# Pit Lab
+
+# Characteristics
+# Fur Color: Varies
+# Eye Color: Varies
+# Fur Length: medium
+# Max Size: 90 lbs
+# Active Level: High
+
+# Behavior
+# Hyper
+# Fast
+# Loving
+# Strong
