a04e6866ec90a3ff8b905182b87fbc9e480af6b9
Allain Dollete <allain.dollete@gmail.com>
2016-09-07 22:52:08 -0400

    Add santa_info method for testing

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,9 +1,21 @@
+# Method Code
+
 class Santa
 
-  def initialize
+  def initialize(gender, ethnicity)
 
     puts "Initializing Santa instance ..."
 
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ['Rudolf', 'Dasher', 'Dancer', 'Prancer', 'Vixen', 'Comet', 'Cupid', 'Donner', 'Blitzen']
+    @age = 0
+
+  end
+
+  def santa_info
+    puts "Gender: #{@gender}"
+    puts "Ethnicity: #{@ethnicity}"
   end
 
   def speak
@@ -15,3 +27,13 @@ class Santa
   end
 
 end
+
+santas = []
+genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+# Driver Code
+santa_con = Santa.new(genders[0], ethnicities[0])
+santa_con.santa_info
+santa_con.speak
+santa_con.eat_milk_and_cookies("Chocolate Chip")
