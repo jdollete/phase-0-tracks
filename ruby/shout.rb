26044df5d72a66686a1476e3d92761a1454ef85e
Allain Dollete <allain.dollete@gmail.com>
2016-09-09 19:22:38 -0400

    Create module with yelling angrily/happily methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,8 +1,16 @@
+# Method Code
+
 module Shout
   def self.yell_angrily(words)
-    words + "!!!" + " :("
+    words.upcase + "!!!" + " :("
   end
 
-  def yelling_happily(words)
-    words + "!!!" + " :)"
+  def self.yell_happily(words)
+    words.upcase + "!!!" + " :)"
+  end
+
 end
+
+# Driver Code
+p Shout.yell_angrily("What in the....")
+p Shout.yell_happily("Congrats")
