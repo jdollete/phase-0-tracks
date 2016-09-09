48221fd172cd1e4aa41549611d856fb0f682968e
Allain Dollete <allain.dollete@gmail.com>
2016-09-09 14:22:30 -0400

    Complete Release 2

--- ruby/calculator/calculator_spec.rb
+++ ruby/calculator/calculator_spec.rb
@@ -28,5 +28,17 @@ describe Calculator do
   it "divides two integers" do
     expect(calculator.divide(4, 2)).to eq 2
   end
-  
+
 end
+
+# Release 2 - Research
+# 1. A few cheat sheet web sites:
+#   - http://cheatrags.com/rspec-matchers
+#   - https://www.relishapp.com/rspec/rspec-expectations/docs/built-in-matchers
+#   - From a quick glance there are many types of matchers that could be used
+# 2. RSPEC Documentation: http://betterspecs.org/
+3. Other matchers are:
+  - Identity [.to be(expected)]
+  - Comparisons [.to be >,<,>=, etc expected]
+  - Truthness [.to be true]
+  - Errors []
