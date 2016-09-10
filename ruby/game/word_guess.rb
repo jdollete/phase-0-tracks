16645c7923e2e281bf12ce40146f5d2b67dbb917
Allain Dollete <allain.dollete@gmail.com>
2016-09-09 21:26:22 -0400

    Initialize pseudocode

---
+++ ruby/game/word_guess.rb
@@ -0,0 +1,36 @@
+# One user can enter a word (or phrase, if you would like your game to support that), and another user attempts to guess the word.
+# Guesses are limited, and the number of guesses available is related to the length of the word.
+# Repeated guesses do not count against the user.
+# The guessing player receives continual feedback on the current state of the word. So if the secret word is "unicorn", the user will
+# start out seeing something like "_ _ _ _ _ _ _", which would become "_ _ _ c _ _ _" after the user enters a guess of "c".
+# The user should get a congratulatory message if they win, and a taunting message if they lose.
+
+# Method Code
+# input: Player 1 User Inputs main word to guess
+# steps:
+        # Ask Player 1 User for word
+        # Store word into a variable
+        # Get length of word and store integer into a variable
+# output: String, integer
+
+# input: Player 2 User Inputs word to guess the main word
+# steps:
+        # Ask player 2 for word input
+        # Store guess word into a variable
+# output:
+
+# input:
+# steps:
+# output:
+
+# input:
+# steps:
+# output:
+
+# input:
+# steps:
+# output:
+
+# input:
+# steps:
+# output:
