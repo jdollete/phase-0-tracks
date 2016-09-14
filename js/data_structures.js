05871350ce90f8741815493e2edd999c460b47b1
Allain Dollete <allain.dollete@gmail.com>
2016-09-14 12:29:09 -0400

    Uncomment first section for submission

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,14 +1,18 @@
-// var color = ["blue", "red", "purple", "pink"];
-// var horse_name = ["Ed", "Sea Biscuit", "Tom", "Mr. H"];
-//
-// color.push("Orange");
-// horse_name.push("Sky");
-// var horses = {};
-// for (var i = 0; i <= color.length - 1; i++){
-// horses[horse_name[i]] = color[i];
-// }
+var color = ["blue", "red", "purple", "pink"];
+var horse_name = ["Ed", "Sea Biscuit", "Tom", "Mr. H"];
 
-// console.log(horses);
+color.push("Orange");
+horse_name.push("Sky");
+var horses = {};
+for (var i = 0; i <= color.length - 1; i++){
+horses[horse_name[i]] = color[i];
+}
+
+console.log(horses);
+
+
+// -----------------------------------------------
+
 
 function Car(year, make, model) {
 	console.log("Our new Car:", this);
