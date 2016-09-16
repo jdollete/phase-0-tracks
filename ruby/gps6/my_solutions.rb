c9676eaaea6565ae015d115b3e7c1d46583ca93b
Allain Dollete <allain.dollete@gmail.com>
2016-09-16 12:43:18 -0400

    Answer reflection section

--- ruby/gps6/my_solutions.rb
+++ ruby/gps6/my_solutions.rb
@@ -37,18 +37,6 @@ class VirusPredictor
         number_of_deaths = (@population * 0.05).floor
     end
 
-    # if @population_density >= 200
-    #   number_of_deaths = (@population * 0.4).floor
-    # elsif @population_density >= 150
-    #   number_of_deaths = (@population * 0.3).floor
-    # elsif @population_density >= 100
-    #   number_of_deaths = (@population * 0.2).floor
-    # elsif @population_density >= 50
-    #   number_of_deaths = (@population * 0.1).floor
-    # else
-    #   number_of_deaths = (@population * 0.05).floor
-    # end
-
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
@@ -61,25 +49,13 @@ class VirusPredictor
     inf = 1.0/0.0
 
     case @population_density
-    when 200..inf then speed += 0.5
-    when 150..199 then speed += 1
-    when 100..149 then speed += 1.5
-    when 50..99 then speed += 2
+      when 200..inf then speed += 0.5
+      when 150..199 then speed += 1
+      when 100..149 then speed += 1.5
+      when 50..99 then speed += 2
       else
         speed += 2.5
     end
-
-    # if @population_density >= 200
-    #   speed += 0.5
-    # elsif @population_density >= 150
-    #   speed += 1
-    # elsif @population_density >= 100
-    #   speed += 1.5
-    # elsif @population_density >= 50
-    #   speed += 2
-    # else
-    #   speed += 2.5
-    # end
 
     puts " and will spread across the state in #{speed} months.\n\n"
 
@@ -108,11 +84,28 @@ end
 # alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 # alaska.virus_effects
 
-
+=begin
 #=======================================================================
 # Reflection Section
 # What are the differences between the two different hash syntaxes shown in the state_data file?
+The first one is a key that is a string which needs hash rockets to set it's value.
+Second sentax is represents a symbol which only needs a colon to set it's value.
+
 # What does require_relative do? How is it different from require?
+Require relative is used for code that you write, require will be used for code that
+someone else wrote. Require relative - you're providing a path to the file relative to
+its place in the directory. Require will load a library for reference in your own code.
+
 # What are some ways to iterate through a hash?
+We could perform a do/loop to iterate through a hash.
+
 # When refactoring virus_effects, what stood out to you about the variables, if anything?
+The methods being called inside virus_effects were passing variables that were already
+instance variables. Which makes the variable available throughout the class and no need
+to repass them.
+
 # What concept did you most solidify in this challenge?
+Iterating through a hash while passing it through a class. Also finally used the case
+statement other than always writing out a conditional statement.
+
+=end
