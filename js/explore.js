2c105f495b9c2ac4329a86a428850c826c7f77b0
Allain Dollete <allain.dollete@gmail.com>
2016-09-17 21:43:19 -0400

    Create algorithm for js

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+// Take word and save it to a variable
+// Split word into an array
+// Iterate through array by moving the first letter to the last until the word is reversed
+
+var greet = "hello";
+
+for (var i = 0, i ) {
+
+}
